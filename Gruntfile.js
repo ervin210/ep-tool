b1f6efa2b65ea207da8d4ad488361d36167570c5
Robert Massaioli <rmassaioli@atlassian.com>
2015-12-10 09:05:16 +1100

    We have a basic express js service running.

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,13 +11,31 @@ module.exports = function(grunt) {
             src: 'src/<%= pkg.name %>.js',
             dest: 'build/<%= pkg.name %>.min.js'
          }
+      },
+      express: {
+         dev: {
+            options: {
+               script: 'main.js'
+            }
+         }
+      },
+      watch: {
+         express: {
+            files: [ '**/*.js' ],
+            tasks: [ 'express:dev' ],
+            options: {
+               spawn: false
+            }
+         }
       }
    });
 
    // Load the plugin that provides the "uglify" task.
    grunt.loadNpmTasks('grunt-contrib-uglify');
+   grunt.loadNpmTasks('grunt-express-server');
+   grunt.loadNpmTasks('grunt-contrib-watch');
 
    // Default task(s).
-   grunt.registerTask('default', ['uglify']);
+   grunt.registerTask('default', ['express:dev', 'watch']);
 
 };
