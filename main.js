373cf4c6b6fd8dbbf2b585ec3b896540ad5446d7
Robert Massaioli <rmassaioli@atlassian.com>
2015-12-10 09:24:39 +1100

    Continuing to improve the application and writing the descriptor.
    Now I just need to start considering the plugin resources.

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
 var express = require('express');
 var app = express();
 
+var hostUrl = process.env.HOST_URL || "http://localhost";
+var serverPort = process.env.PORT || 3000;
+
 app.get('/', function (req, res) {
    res.send('Hello World!');
 });
 
-var server = app.listen(3000, function () {
+app.get('/jira/atlassian-connect.json', function(req, res) {
+   res.json({
+      name: 'Entity Property Tool',
+      key: 'com.atlassian.connect.entity-property-tool',
+      version: "1.0",
+      description: 'An Atlassian Connect addon that allows every user to manipulate entity properties.',
+      vendor: {
+         name: 'Atlassian',
+         url: 'http://www.atlassian.com'
+      },
+      baseUrl: hostUrl + ":" + serverPort,
+      authentication: {
+         type: "none"
+      },
+      scopes: ["read", "write"],
+      modules: {
+      }
+   });
+})
+
+var server = app.listen(serverPort, function () {
    var host = server.address().address;
    var port = server.address().port;
 
